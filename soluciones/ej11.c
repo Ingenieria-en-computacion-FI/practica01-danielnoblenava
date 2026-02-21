7dc8c3211bbe2defa18658e03f76ce8bbf1d490c
danielnoblenava <danielnoble290@gmail.com>
2026-02-21 17:05:27 -0600

    Implement logic to print calculated values
    Fix loop to correctly iterate and print values.

--- soluciones/ej11.c
+++ soluciones/ej11.c
@@ -1,6 +1,20 @@
 
 #include <stdio.h>
+
+/*
+  Tamano del tipo: 8 bytes
+  Tipo posible: long long
+  Formula usada: 1000+i*sizeof(a[i])
+*/
+
 int main(){
     // Resolver ejercicio 11
-    return 0;
+	long long a[3];
+	
+	for(int i = 0; i < 3; i++){
+		printf("\n&a[%d] = %d", i, 1000+i*sizeof(a[i]));
+	}
+	printf("\n");
+	
+	return 0;
 }
