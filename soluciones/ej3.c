29412d3cf9e16fb58c0994b944ce598f356a8d53
danielnoblenava <danielnoble290@gmail.com>
2026-02-21 17:00:47 -0600

    Print memory addresses of arrays a, b, and c

--- soluciones/ej3.c
+++ soluciones/ej3.c
@@ -1,6 +1,22 @@
-
 #include <stdio.h>
+
 int main(){
-    // Resolver ejercicio 3
-    return 0;
+	//Resolver ejercicio 3
+    
+	int a[3], b[3], c[3];
+	printf("\nDirecciones de a: ");
+	for(int i = 0; i < 3; i++)
+		printf("\na[%d]: %p", i,  &a[i]);
+	
+	printf("\nDirecciones de b: ");
+	for(int i = 0; i < 3; i++)
+		printf("\nb[%d]: %p", i, &b[i]);
+	
+	printf("\nDirecciones de c: ");
+	for (int i = 0; i < 3; i++)
+		printf("\nc[%d]: %p", i, &c[i]);
+	printf("\n");
+        return 0;
 }
+
+
