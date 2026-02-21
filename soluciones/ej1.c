b0bc2974e4e8047176f2d86025eb8da2adfa5207
danielnoblenava <danielnoble290@gmail.com>
2026-02-21 16:59:47 -0600

    Implement address printing for array elements
    Added a loop to print the addresses of array elements.

--- soluciones/ej1.c
+++ soluciones/ej1.c
@@ -1,6 +1,18 @@
-
 #include <stdio.h>
+
+/*
+  La diferencia entre direcciones consecutivas: 4 bytes
+  Formula general: Base + i x tam_tip
+*/
+
 int main(){
-    // Resolver ejercicio 1
-    return 0;
+	//Resolver ejercicio 1
+	int a[6];
+
+	for(int i = 0; i < 6; i++){
+		printf("\nDireccion: %p", &a[i]);
+	}
+
+	return 0;
 }
+
