3c791fe76a1fda6ce7e7e14003ec8d6aa85f24d3
danielnoblenava <danielnoble290@gmail.com>
2026-02-21 17:00:19 -0600

    Implement memory address printing for array elements
    Print memory addresses of array elements a[4] and a[5].

--- soluciones/ej2.c
+++ soluciones/ej2.c
@@ -1,6 +1,19 @@
 
 #include <stdio.h>
+
+/*
+  La dirección a[5] es mayor que a[4] porque esta más "arriba" en memor
+  Direccion de a[4] = 0x7fffc69eba50
+  Direccion de a[5] = 0x7fffc69eba54
+*/
+
 int main(){
-    // Resolver ejercicio 2
-    return 0;
+    	int a[6];
+	printf("\n%p", &a[4]);
+	printf("\n%p", &a[5]);
+        return 0;
 }
+
+
+
+
