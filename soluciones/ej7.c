efab129351c4f6deed4fab8b823d3f961985a125
danielnoblenava <danielnoble290@gmail.com>
2026-02-21 17:03:08 -0600

    Implement memory address printing for array elements
    Add memory address comparisons for 2D array elements

--- soluciones/ej7.c
+++ soluciones/ej7.c
@@ -1,6 +1,12 @@
-
 #include <stdio.h>
+
+/*
+  La direccion m[1][0] es mayor que m[0][3], porque esta mÃs 
+  arriba en la memoria.
+*/
 int main(){
-    // Resolver ejercicio 7
-    return 0;
+	int m[2][4];
+	printf("\nm[0][3] = %p ", &m[0][3]);
+	printf("\nm[1][0] = %p", &m[0][1]);
+	printf("\n");
 }
